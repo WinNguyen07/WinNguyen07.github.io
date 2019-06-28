c0aa958627dded5f2a820c65449bdd834744f17e
WinNguyen07 <43165247+WinNguyen07@users.noreply.github.com>
2019-06-29 00:26:10 +0700

    Update openStream.js

--- src/openStream.js
+++ src/openStream.js
@@ -1,7 +1,7 @@
 const playVideo = require('./playVideo');
 
 function openStream(callback){
-    navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+    navigator.mediaDevices.getUserMedia({ audio: false, video: true })
     .then(stream => {
         callback(stream);        
     })
