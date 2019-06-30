b476a1b8c9c214648445d5ea48309795a4064d18
WinNguyen07 <43165247+WinNguyen07@users.noreply.github.com>
2019-06-30 13:15:52 +0700

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -16,6 +16,30 @@ function getPeer() {
     return id;    
 }
 
+//This example shows you how simple it is to get your ICE credentials
+
+var https = require("https");
+var options = {
+    host: "global.xirsys.net",
+    path: "/_turn/default",
+    method: "PUT",
+    headers: {
+       "Authorization": "Basic " + Buffer.from("winhynguyen:bcf7661e-9ae6-11e9-b18c-0242ac110007").toString("base64"),
+    }
+};
+
+var httpreq = https.request(options, function(httpres) {
+    var str = "";
+    httpres.on("data", function(data){ str += data; });
+    httpres.on("error", function(e){ console.log("error: ",e); });
+    httpres.on("end", function(){
+    console.log("response: ", str);
+    });
+});
+
+httpreq.end();
+
+
 const peer = new Peer(getPeer());
 
 $('#btnCall').click(() => {
